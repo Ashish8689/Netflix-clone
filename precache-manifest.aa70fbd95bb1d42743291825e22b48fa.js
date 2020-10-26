f0a6fd4f78087674b66771e80c9dd95a27431eb9
Ashish <ag939431@gmail.com>
2020-10-26 18:27:42 +0530

    Updates

---
+++ precache-manifest.aa70fbd95bb1d42743291825e22b48fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96c9954ed3d2fd825a8fdeb639503682",
+    "url": "/Netflix-clone/index.html"
+  },
+  {
+    "revision": "5c5e98df339d85a7c181",
+    "url": "/Netflix-clone/static/css/main.6c3c65d5.chunk.css"
+  },
+  {
+    "revision": "b2b0bb58b483c1dea78d",
+    "url": "/Netflix-clone/static/js/2.c7660281.chunk.js"
+  },
+  {
+    "revision": "8c531855ea370064c601b9c584684a5a",
+    "url": "/Netflix-clone/static/js/2.c7660281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c5e98df339d85a7c181",
+    "url": "/Netflix-clone/static/js/main.21011728.chunk.js"
+  },
+  {
+    "revision": "37005dd9bfd48c744798",
+    "url": "/Netflix-clone/static/js/runtime-main.2122e3aa.js"
+  }
+]);
